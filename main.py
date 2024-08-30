a0646d2f4f76d2ea14f3c36460a508294cd4d0fa
MohammedAlamri <m.s.alamri22@gmail.com>
2024-08-31 00:54:41 +0300

    .

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import joblib
 
 model = joblib.load('dbscan.joblib')
 scaler = joblib.load('scaler1.joblib')
- 
+
 app = FastAPI()
 
 class InputFeatures(BaseModel):
@@ -23,12 +23,8 @@ def preprocessing(input_features: InputFeatures):
 def read_root():
     return {"message": "Welcome to Tuwaiq Academy"}
 
-@app.get("/try/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
 @app.post("/predict")
 async def predict(input_features: InputFeatures):
     data = preprocessing(input_features)
-    y_pred = model.predict(data)  # Use predict instead of fit_predict
+    y_pred = model.predict(data)  # Ensure the model supports predict method
     return {"cluster": int(y_pred[0])}
